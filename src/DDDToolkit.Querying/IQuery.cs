5e6614d16b8e2b72e203809d51823976b4dc33a3
codepb <paul.j.burgess@outlook.com>
2019-02-20 21:21:01 +0000

    Initial commit

---
+++ src/DDDToolkit.Querying/IQuery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace DDDToolkit.Querying
+{
+    public interface IQuery<T>
+    {
+        IQuery<T> AndSatisfying(IQuery<T> query);
+        IQuery<T> OrSatisfying(IQuery<T> query);
+        Expression<Func<T, bool>> AsExpression();
+        bool IsSatisfiedBy(T subject);
+    }
+}
