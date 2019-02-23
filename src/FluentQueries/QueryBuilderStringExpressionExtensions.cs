8c8fc2ba420b49742687ec2158c280a2a1c47bfd
codepb <paul.j.burgess@outlook.com>
2019-02-23 22:05:43 +0000

    Adding not queries of string expression extensions

--- src/FluentQueries/QueryBuilderStringExpressionExtensions.cs
+++ src/FluentQueries/QueryBuilderStringExpressionExtensions.cs
@@ -6,23 +6,45 @@ namespace FluentQueries
 {
     public static class QueryBuilderStringExpressionExtensions
     {
-        private static Query<T> BuildQuery<T>(string methodName, QueryBuilderExpression<T, string> queryBuilderExpression, string other)
+        private static MethodCallExpression CreateExpression<T>(string methodName, QueryBuilderExpression<T, string> queryBuilderExpression, string other)
         {
             var otherEntity = Expression.Constant(other, typeof(string));
             var method = typeof(string).GetMethod(methodName, new[] { typeof(string) });
             var expression = Expression.Call(queryBuilderExpression._expression.Body, method, otherEntity);
+            return expression;
+        }
+
+        private static Query<T> BuildQuery<T>(string methodName, QueryBuilderExpression<T, string> queryBuilderExpression, string other)
+        {
+            var expression = CreateExpression(methodName, queryBuilderExpression, other);
             var lambda = Expression.Lambda<Func<T, bool>>(expression, queryBuilderExpression._expression.Parameters);
+            return queryBuilderExpression._continueWith(new Query<T>(lambda));
+        }
+
+        private static Query<T> BuildNotQuery<T>(string methodName, QueryBuilderExpression<T, string> queryBuilderExpression, string other)
+        {
+            var expression = CreateExpression(methodName, queryBuilderExpression, other);
+            var lambda = Expression.Lambda<Func<T, bool>>(Expression.Not(expression), queryBuilderExpression._expression.Parameters);
             return queryBuilderExpression._continueWith(new Query<T>(lambda));
         }
 
         public static Query<T> Containing<T>(this QueryBuilderExpression<T, string> queryBuilderExpression, string other)
             => BuildQuery("Contains", queryBuilderExpression, other);
 
+        public static Query<T> NotContaining<T>(this QueryBuilderExpression<T, string> queryBuilderExpression, string other)
+            => BuildNotQuery("Contains", queryBuilderExpression, other);
+
         public static Query<T> StartingWith<T>(this QueryBuilderExpression<T, string> queryBuilderExpression, string other)
             => BuildQuery("StartsWith", queryBuilderExpression, other);
 
+        public static Query<T> NotStartingWith<T>(this QueryBuilderExpression<T, string> queryBuilderExpression, string other)
+            => BuildNotQuery("StartsWith", queryBuilderExpression, other);
+
         public static Query<T> EndingWith<T>(this QueryBuilderExpression<T, string> queryBuilderExpression, string other)
             => BuildQuery("EndsWith", queryBuilderExpression, other);
+
+        public static Query<T> NotEndingWith<T>(this QueryBuilderExpression<T, string> queryBuilderExpression, string other)
+            => BuildNotQuery("EndsWith", queryBuilderExpression, other);
 
         public static Query<T> NullOrWhitespace<T>(this QueryBuilderExpression<T, string> queryBuilderExpression)
         {
@@ -32,11 +54,27 @@ namespace FluentQueries
             return queryBuilderExpression._continueWith(new Query<T>(lambda));
         }
 
+        public static Query<T> NotNullOrWhitespace<T>(this QueryBuilderExpression<T, string> queryBuilderExpression)
+        {
+            var method = typeof(string).GetMethod("IsNullOrWhiteSpace", BindingFlags.Public | BindingFlags.Static);
+            var expression = Expression.Call(method, queryBuilderExpression._expression.Body);
+            var lambda = Expression.Lambda<Func<T, bool>>(Expression.Not(expression), queryBuilderExpression._expression.Parameters);
+            return queryBuilderExpression._continueWith(new Query<T>(lambda));
+        }
+
         public static Query<T> NullOrEmpty<T>(this QueryBuilderExpression<T, string> queryBuilderExpression)
         {
             var method = typeof(string).GetMethod("IsNullOrEmpty", BindingFlags.Public | BindingFlags.Static);
             var expression = Expression.Call(method, queryBuilderExpression._expression.Body);
             var lambda = Expression.Lambda<Func<T, bool>>(expression, queryBuilderExpression._expression.Parameters);
+            return queryBuilderExpression._continueWith(new Query<T>(lambda));
+        }
+
+        public static Query<T> NotNullOrEmpty<T>(this QueryBuilderExpression<T, string> queryBuilderExpression)
+        {
+            var method = typeof(string).GetMethod("IsNullOrEmpty", BindingFlags.Public | BindingFlags.Static);
+            var expression = Expression.Call(method, queryBuilderExpression._expression.Body);
+            var lambda = Expression.Lambda<Func<T, bool>>(Expression.Not(expression), queryBuilderExpression._expression.Parameters);
             return queryBuilderExpression._continueWith(new Query<T>(lambda));
         }
     }
--- tests/FluentQueries.Tests/Query_StringExtensionsShould.cs
+++ tests/FluentQueries.Tests/Query_StringExtensionsShould.cs
@@ -29,6 +29,21 @@ namespace FluentQueries.Tests
         }
 
         [Fact]
+        public void NotStartsWithCorrectlyReturnsTrue()
+        {
+            var query = Query<TestEntity>.Has(a => a.A).NotStartingWith("b").And.Has(a => a.B).NotStartingWith("e");
+
+            _testEntity.Satisfies(query).Should().BeTrue();
+        }
+        [Fact]
+        public void NotStartsWithCorrectlyReturnsFalse()
+        {
+            var query = Query<TestEntity>.Has(a => a.A).NotStartingWith("a").And.Has(a => a.B).NotStartingWith("e");
+
+            _testEntity.Satisfies(query).Should().BeFalse();
+        }
+
+        [Fact]
         public void EndsWithCorrectlyReturnsTrue()
         {
             var query = Query<TestEntity>.Has(a => a.A).EndingWith("c").And.Has(a => a.B).EndingWith("ef");
@@ -40,6 +55,22 @@ namespace FluentQueries.Tests
         public void EndsWithCorrectlyReturnsFalse()
         {
             var query = Query<TestEntity>.Has(a => a.A).EndingWith("ab").And.Has(a => a.B).EndingWith("ef");
+
+            _testEntity.Satisfies(query).Should().BeFalse();
+        }
+
+        [Fact]
+        public void NotEndsWithCorrectlyReturnsTrue()
+        {
+            var query = Query<TestEntity>.Has(a => a.A).NotEndingWith("b").And.Has(a => a.B).NotEndingWith("de");
+
+            _testEntity.Satisfies(query).Should().BeTrue();
+        }
+
+        [Fact]
+        public void NotEndsWithCorrectlyReturnsFalse()
+        {
+            var query = Query<TestEntity>.Has(a => a.A).NotEndingWith("ab").And.Has(a => a.B).NotEndingWith("ef");
 
             _testEntity.Satisfies(query).Should().BeFalse();
         }
@@ -61,6 +92,22 @@ namespace FluentQueries.Tests
         }
 
         [Fact]
+        public void NotContainingCorrectlyReturnsTrue()
+        {
+            var query = Query<TestEntity>.Has(a => a.A).NotContaining("ef").And.Has(a => a.B).NotContaining("a");
+
+            _testEntity.Satisfies(query).Should().BeTrue();
+        }
+
+        [Fact]
+        public void NotContainingCorrectlyReturnsFalse()
+        {
+            var query = Query<TestEntity>.Has(a => a.A).NotContaining("ab").And.Has(a => a.B).NotContaining("fg");
+
+            _testEntity.Satisfies(query).Should().BeFalse();
+        }
+
+        [Fact]
         public void NullOrWhitespaceCorrectlyReturnsTrue()
         {
             var query = Query<string>.Is.NullOrWhitespace();
@@ -74,6 +121,22 @@ namespace FluentQueries.Tests
             var query = Query<string>.Is.NullOrWhitespace();
 
             "a".Satisfies(query).Should().BeFalse();
+        }
+
+        [Fact]
+        public void NotNullOrWhitespaceCorrectlyReturnsTrue()
+        {
+            var query = Query<string>.Is.NotNullOrWhitespace();
+
+            "c".Satisfies(query).Should().BeTrue();
+        }
+
+        [Fact]
+        public void NotNullOrWhitespaceCorrectlyReturnsFalse()
+        {
+            var query = Query<string>.Is.NotNullOrWhitespace();
+
+            "    ".Satisfies(query).Should().BeFalse();
         }
 
         [Fact]
@@ -91,6 +154,23 @@ namespace FluentQueries.Tests
             var query = Query<string>.Is.NullOrEmpty();
 
             "    ".Satisfies(query).Should().BeFalse();
+        }
+
+        [Fact]
+        public void NotNullOrEmptyCorrectlyReturnsTrue()
+        {
+            var query = Query<string>.Is.NotNullOrEmpty();
+
+            "   ".Satisfies(query).Should().BeTrue();
+        }
+
+        [Fact]
+        public void NotNullOrEmptyCorrectlyReturnsFalse()
+        {
+            var query = Query<string>.Is.NotNullOrEmpty();
+
+            "".Satisfies(query).Should().BeFalse();
+            query.IsSatisfiedBy(null).Should().BeFalse();
         }
     }
 }
