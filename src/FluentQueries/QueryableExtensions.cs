818a3d17995d60a7df04ed48386e10404bbf0e93
codepb <paul.j.burgess@outlook.com>
2019-04-09 09:05:35 +0100

    Adding WhereSatisfies method to IQueryable<T> (#14)

--- src/FluentQueries/QueryableExtensions.cs
+++ src/FluentQueries/QueryableExtensions.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Linq;
 using System.Linq.Expressions;
 
 namespace FluentQueries
 {
-    internal static class QueryableExtensions
+    /// <summary>
+    /// Extensions for Queryables
+    /// </summary>
+    public static class QueryableExtensions
     {
         internal static Expression<Func<TResult, bool>> WithParameter<TResult, TSource>(this Expression<Func<TSource, bool>> source, Expression<Func<TResult, TSource>> selector)
         {
@@ -12,6 +16,18 @@ namespace FluentQueries
             // This will be the new body of the expression
             var newExpressionBody = replaceParameter.Visit(source.Body);
             return Expression.Lambda<Func<TResult, bool>>(newExpressionBody, selector.Parameters);
+        }
+
+        /// <summary>
+        /// Filters the queryable to only those that satisfy the supplied query.
+        /// </summary>
+        /// <typeparam name="T">The type of object.</typeparam>
+        /// <param name="source">The source queryable.</param>
+        /// <param name="query">The query to test objects against.</param>
+        /// <returns>A queryable that tests objects against the supplied query.</returns>
+        public static IQueryable<T> WhereSatisfies<T>(this IQueryable<T> source, Query<T> query)
+        {
+            return source.Where(query.AsExpression());
         }
     }
 }
